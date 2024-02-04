10d7a55c45a523300aff1957ad08a46377522177
archeryue <archeryue7@gmail.com>
2024-02-04 19:04:25 +0800

    ADD: task-list project dir

---
+++ tasks/task-app/src/TaskList.vue
@@ -0,0 +1,47 @@
+<template>
+    <div>
+        <h2>{{ listTitle }}</h2>
+
+        <!-- 显示任务列表 -->
+        <ul>
+            <li v-for="task in tasks" :key="task.id">
+                {{ task.name }} - {{ task.completed ? '已完成' : '未完成' }}
+                <button @click="toggleTaskStatus(task)">切换状态</button>
+                <button @click="removeTask(task)">删除</button>
+            </li>
+        </ul>
+
+        <!-- 添加新任务 -->
+        <div>
+            <input v-model="newTask" @keyup.enter="addTask" placeholder="添加新任务">
+            <button @click="addTask">添加</button>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        props: ['tasks', 'listTitle'],
+        data() {
+            return {
+                newTask: ''
+            };
+        },
+        methods: {
+            toggleTaskStatus(task) {
+                task.completed = !task.completed;
+            },
+            removeTask(task) {
+                const index = this.tasks.indexOf(task);
+                if (index > -1) {
+                    this.tasks.splice(index, 1);
+                }
+            },
+            addTask() {
+                if (this.newTask.trim() === '') return;
+                this.tasks.push({ id: this.tasks.length + 1, name: this.newTask, completed: false });
+                this.newTask = '';
+            }
+        }
+    };
+</script>
