ad02b0e4513312490199c281117ed528a62984a8
archeryue <archeryue7@gmail.com>
2024-02-11 18:45:26 +0800

    ADD: todo-list project initiation

---
+++ tasks/todo-list/src/App.vue
@@ -0,0 +1,22 @@
+<script setup>
+</script>
+
+<template>
+  <header>
+    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
+  </header>
+
+  <main>
+  </main>
+</template>
+
+<style scoped>
+header {
+  line-height: 1.5;
+}
+
+.logo {
+  display: block;
+  margin: 0 auto 2rem;
+}
+</style>
---
+++ tasks/todo-list/src/components/ListItem.vue
@@ -0,0 +1,72 @@
+<template>
+    <li>
+        <label>
+            <input type="checkbox" :checked="item.done" @change="toggleTodo(item.id)" />
+            <span>{{ item.text }}</span>
+        </label>
+    </li>
+    <button class="btn btn-danger" @click="removeTodo(item.id)">Remove</button>
+</template>
+
+<script setup>
+    const props = defineProps({
+        item: {
+            type: Object,
+            required: true
+        }
+    });
+
+    function toggleTodo(id) {
+        this.toggleTodo(id);
+    }
+
+    function removeTodo(id) {
+        if (confirm('Are you sure you want to delete this item?')) {
+            this.removeTodo(id);
+        }
+    };
+</script>
+
+<style scoped>
+    li {
+        list-style: none;
+        height: 36px;
+        line-height: 36px;
+        padding: 0 5px;
+        border-bottom: 1px solid #dddddd;
+    }
+
+    li label {
+        float: left;
+        cursor: pointer;
+    }
+
+    li label li input {
+        vertical-align: middle;
+        margin-right: 6px;
+        position: relative;
+        top: -1px;
+    }
+
+    li button {
+        float: right;
+        display: none;
+        margin-top: 3px;
+    }
+
+    li:before {
+        content: initial;
+    }
+
+    li:last-child {
+        border-bottom: none;
+    }
+
+    li:hover {
+        background-color: lightskyblue;
+    }
+
+    li:hover button {
+        display: block;
+    }
+</style>
