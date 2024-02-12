402d30b447d346470cc6f6f030608c260723bde1
archeryue <archeryue7@gmail.com>
2024-02-12 19:06:47 +0800

    MOD: seems css in ListItem doesnt work

--- tasks/todo-list/src/components/ListItem.vue
+++ tasks/todo-list/src/components/ListItem.vue
@@ -9,66 +9,66 @@
 </template>
 
 <script setup>
-    const props = defineProps({
-        item: {
-            type: Object,
-            required: true
-        },
-        toggleTodo: Function,
-        removeTodo: Function
-    });
+const props = defineProps({
+    item: {
+        type: Object,
+        required: true
+    },
+    toggleTodo: Function,
+    removeTodo: Function
+});
 
-    function toggleItem(id) {
-        props.toggleTodo(id);
+function toggleItem(id) {
+    props.toggleTodo(id);
+}
+
+function removeItem(id) {
+    if (confirm('Are you sure you want to delete this item?')) {
+        props.removeTodo(id);
     }
-
-    function removeItem(id) {
-        if (confirm('Are you sure you want to delete this item?')) {
-            props.removeTodo(id);
-        }
-    };
+};
 </script>
 
 <style scoped>
-    li {
-        list-style: none;
-        height: 36px;
-        line-height: 36px;
-        padding: 0 5px;
-        border-bottom: 1px solid #dddddd;
-    }
+li {
+    list-style: none;
+    height: 36px;
+    line-height: 36px;
+    padding: 0 5px;
+    border-bottom: 1px solid #dddddd;
+}
 
-    li label {
-        float: left;
-        cursor: pointer;
-    }
+li label {
+    float: left;
+    cursor: pointer;
+}
 
-    li label li input {
-        vertical-align: middle;
-        margin-right: 6px;
-        position: relative;
-        top: -1px;
-    }
+li label li input {
+    vertical-align: middle;
+    margin-right: 6px;
+    position: relative;
+    top: -1px;
+}
 
-    li button {
-        float: right;
-        display: none;
-        margin-top: 3px;
-    }
+li button {
+    float: right;
+    display: none;
+    margin-top: 3px;
+}
 
-    li:before {
-        content: initial;
-    }
+li:before {
+    content: initial;
+}
 
-    li:last-child {
-        border-bottom: none;
-    }
+li:last-child {
+    border-bottom: none;
+}
 
-    li:hover {
-        background-color: lightskyblue;
-    }
+li:hover {
+    background-color: lightskyblue;
+}
 
-    li:hover button {
-        display: block;
-    }
+li:hover button {
+    display: block;
+}
 </style>
