2e100e9798d3f8bf6be6ba0791ac3447c58b16a8
archeryue <archeryue7@gmail.com>
2024-07-10 00:10:41 +0800

    ADD: grid test

--- ui-tasks/todo-list/src/App.vue
+++ ui-tasks/todo-list/src/App.vue
@@ -2,4 +2,10 @@
   <h1 class="text-3xl font-bold underline">
     Hello world!
   </h1>
+  <div class="grid grid-cols-1 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-4">
+    <div class="w-full h-24 bg-red-500">Item 1</div>
+    <div class="w-full h-24 bg-blue-500">Item 2</div>
+    <div class="w-full h-24 bg-green-500">Item 3</div>
+    <div class="w-full h-24 bg-yellow-500">Item 4</div>
+  </div>
 </template>
