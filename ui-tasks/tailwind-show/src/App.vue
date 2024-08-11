697cc2d2030cd7329375c20460d27b80875d1d9b
archeryue <archeryue7@gmail.com>
2024-08-11 17:04:06 +0800

    MOD: make the aside inside the division

--- ui-tasks/tailwind-show/src/App.vue
+++ ui-tasks/tailwind-show/src/App.vue
@@ -58,7 +58,7 @@
 
                 <!-- Aside Panel -->
                 <aside v-if="isAsideVisible" @click.stop
-                    class="fixed top-0 right-0 w-64 h-full bg-gray-800 text-white shadow-lg transform transition-transform duration-300"
+                    class="absolute top-0 right-0 w-64 h-full bg-gray-800 text-white shadow-lg transform transition-transform duration-300"
                     :class="{ 'translate-x-full': !isAsideVisible, 'translate-x-0': isAsideVisible }">
                     <button @click="hideAside" class="absolute top-4 right-4 text-white">Close</button>
                     <div class="p-4">
