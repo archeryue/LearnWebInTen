9d43216070dc36d2221933a838090ba073773c92
archeryue <archeryue7@gmail.com>
2024-08-18 14:27:58 +0800

    FIX: typo in tailwind-show

--- ui-tasks/tailwind-show/src/App.vue
+++ ui-tasks/tailwind-show/src/App.vue
@@ -5,7 +5,7 @@
             <h-1 class="text-3xl font-bold underline">
                 Hello world!
             </h-1>
-            <div class="grid grid-cols-3 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-4">
+            <div class="grid grid-cols-1 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-4">
                 <div class="w-full h-26 bg-red-500">Item 1</div>
                 <div class="w-full h-26 bg-blue-500">Item 2</div>
                 <div class="w-full h-26 bg-green-500">Item 3</div>
